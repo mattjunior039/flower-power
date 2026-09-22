31a5551ca6874613e34efb44e5ec701f892c5196
mattjunior039 <matthew.flowerhill@gmail.com>
2026-09-21 22:16:34 -0700

    fix: use lowercase mangayomiMihonBridge for capability check

--- lib/services/m_extension_server.dart
+++ lib/services/m_extension_server.dart
@@ -111,7 +111,7 @@ class MExtensionServerPlatform {
             .get(Uri.parse("$baseUrl/capabilities"))
             .timeout(const Duration(milliseconds: 500));
         if (res.statusCode == 200 &&
-            res.body.contains('"MangayomiMihonBridge"')) {
+            res.body.contains('"mangayomiMihonBridge"')) {
           return true;
         }
       } catch (_) {}
